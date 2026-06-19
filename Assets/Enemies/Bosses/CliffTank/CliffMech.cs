d615ee63164c7e708b7a8f4f6558ba15da4b8934
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-06-18 20:03:43 -0400

    fully animated tank started rescripting the boss ai

---
+++ Assets/Enemies/Bosses/CliffTank/CliffMech.cs
@@ -0,0 +1,229 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CliffMech : MonoBehaviour
+{
+    private CliffCutscene cliffCutscene;
+    private Rigidbody2D bossRig;
+    private SpriteRenderer bossSprite;
+    private Transform druidTransform;
+    public float movementSpeed = 2f;
+    [SerializeField] private GameObject rocket;
+    private bool rocketCooldown = false;
+    private bool isShooting = false;
+    private bool isDashing = false;
+    [SerializeField] private float timeBetweenRockets = 0.2f;
+    [SerializeField] private float rocketAmount;
+    private List<GameObject> rocketList = new List<GameObject>();
+    private List<Transform> rocketHitPositions = new List<Transform>();
+    [SerializeField] private float rocketCooldownTime = 6f;
+    [SerializeField] private Transform normalPos;
+    [SerializeField] private float offset = 3f;
+    [SerializeField] private bool mechDone = false;
+    [SerializeField] private bool dashCd = false;
+    [SerializeField] private float dashCooldownTime = 5f;
+    [SerializeField] private float dashDistance = 3f;
+    [SerializeField] private float dashMovementSpeed = 4f;
+    DruidUI druidUI;
+    [SerializeField] private float timeConstraint = 180f;
+    [SerializeField] private float dashInFrontDetection = 2f;
+    private Animator mechAnimator;
+    private bool startedDash = false;
+    EnemyDamage damage;
+    public bool Dead => damage.dead;
+    
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        cliffCutscene = GameObject.Find("Cutscene").GetComponent<CliffCutscene>();
+        mechAnimator = GetComponent<Animator>();
+        bossRig = GetComponent<Rigidbody2D>();
+        druidUI = player.GetComponent<DruidUI>();
+        druidTransform = player.GetComponent<Transform>();
+        bossSprite = GetComponent<SpriteRenderer>();
+        damage = GetComponent<EnemyDamage>();
+    }
+    private void FixedUpdate()
+    {
+        if (bossRig.bodyType == RigidbodyType2D.Dynamic && !mechDone)
+        {
+            Vector2 movePos = new Vector2(druidTransform.position.x, gameObject.transform.position.y) + new Vector2((bossSprite.flipX ? -1 : 1) * offset, 0);
+            if (!isDashing && !isShooting)
+            {
+                Debug.Log("Moving");
+
+                Vector2 direction = (movePos - bossRig.position).normalized;
+                bossRig.linearVelocity = direction * movementSpeed;
+                mechAnimator.SetFloat("XVelo", bossRig.linearVelocity.x);
+            }
+        } 
+    }
+
+    float t = 0;
+    private void Update()
+    {
+        if (bossRig.bodyType == RigidbodyType2D.Dynamic && !mechDone)
+        {
+            t += Time.deltaTime;
+            var druidPos = gameObject.transform.position.x - druidTransform.position.x;
+            if (damage.health <= 90 || druidUI.health == 1 || t >= timeConstraint)
+            {
+                bossRig.linearVelocity = Vector2.zero;
+                mechDone = true;
+                cliffCutscene.MechEnd();
+            }
+            if (druidPos < 0)
+            {
+                bossSprite.flipX = true;
+            }
+            else
+            {
+                bossSprite.flipX = false;
+            }
+
+            if (!rocketCooldown && !isDashing)
+            {
+                StartCoroutine(RocketShoot());
+            }
+
+            if (!dashCd && !isShooting)
+            {
+                if (Vector2.Distance(druidTransform.position, transform.position) <= dashDistance)
+                {
+                    Debug.Log("Dashing!");
+                    StartCoroutine(DashRoutine());
+                }
+            }
+
+            if (isDashing && startedDash)
+            {
+                var dashDir = bossSprite.flipX ? 1 : -1;
+                RaycastHit2D wallCheck = Physics2D.Raycast(transform.position, new Vector2(dashDir, 0), dashInFrontDetection, LayerMask.GetMask("Ground"));
+                if (wallCheck)
+                {
+                    StartCoroutine(StopDash());
+                }
+            }
+        }
+    }
+
+    private IEnumerator DashRoutine()
+    {
+        dashCd = true;
+        isDashing = true;
+        mechAnimator.SetTrigger("Dash");
+        yield return new WaitUntil(() => mechAnimator.GetCurrentAnimatorStateInfo(0).IsName("CliffTankDashing"));
+        var dashDir = bossSprite.flipX ? 1 : -1;
+        startedDash = true;
+        bossRig.AddForceX(dashMovementSpeed, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1.5f);
+        if (startedDash == true)
+        {
+            StartCoroutine(StopDash());
+        } 
+    }
+
+    private IEnumerator StopDash()
+    {
+        bossRig.linearVelocity = Vector2.zero;
+        startedDash = false;
+        mechAnimator.SetTrigger("StopDash");
+        yield return new WaitUntil(() => mechAnimator.GetCurrentAnimatorStateInfo(0).IsName("CliffTankIdle"));
+        yield return new WaitForSeconds(1.5f);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldownTime);
+        dashCd = false;
+    }
+
+    private IEnumerator RocketShoot()
+    {
+        isShooting = true;
+        rocketCooldown = true;
+        mechAnimator.SetTrigger("Shoot");
+        for (int i = 0; i < rocketAmount; i++)
+        {
+            if (mechDone) break;
+            Debug.Log("Shooting");
+            var newRocket = Instantiate(rocket);
+            newRocket.SetActive(true);
+            newRocket.transform.position = normalPos.position;
+            newRocket.transform.rotation = Quaternion.Euler(0, 0, 90);
+            rocketList.Add(newRocket);
+            rocketHitPositions.Add(druidTransform);
+            StartCoroutine(RocketMove(newRocket, druidTransform));
+            yield return new WaitForSeconds(timeBetweenRockets);
+        }
+        mechAnimator.SetTrigger("StopShoot");
+        yield return new WaitForSeconds(1f);
+        isShooting = false;
+        yield return new WaitForSeconds(rocketCooldownTime);
+        rocketCooldown = false;
+    }
+
+    private IEnumerator RocketMove(GameObject rocket, Transform rocketHitPos)
+    {
+        var rocketLine = rocket.GetComponent<LineRenderer>();
+        rocketLine.positionCount = 2;
+        float launchTime = 1f;
+
+        Vector2 startPos = rocket.transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(rocketHitPos.position, Vector2.down, 50f, LayerMask.GetMask("Ground"));
+
+        float t = 0;
+        float launchHeight = 20;
+        Vector2 launchPos = new Vector2(rocket.transform.position.x, rocket.transform.position.y + launchHeight);
+        while (t < launchTime)
+        {
+            t += Time.deltaTime;
+            rocket.transform.position = Vector2.Lerp(startPos, launchPos, t / launchTime);
+            yield return null;
+        }
+
+        Vector2 targetPos = rocketHitPos.position;
+
+        rocket.transform.rotation = Quaternion.Euler(0, 0, -90f);
+        float speed = 5f;
+
+        while (Mathf.Abs(druidTransform.position.x - rocket.transform.position.x) > 0.5f)
+        {
+            RaycastHit2D beamCast = Physics2D.Raycast(rocket.transform.position, Vector2.down, 100, LayerMask.GetMask("Ground"));
+            rocketLine.SetPosition(0, rocket.transform.position);
+            rocketLine.SetPosition(1, beamCast.point);
+            Vector2 rotationPos = new Vector2(druidTransform.position.x, rocket.transform.position.y);
+            rocket.transform.position = Vector2.MoveTowards(rocket.transform.position, rotationPos, speed * Time.deltaTime);
+
+            yield return null;
+            targetPos = beamCast.point;
+
+        }
+
+        int flashCount = 5;
+        for (int i = 0; i < flashCount; i++)
+        {
+            rocketLine.startColor = Color.white;
+            rocketLine.endColor = Color.white;
+            yield return new WaitForSeconds(0.1f);
+            rocketLine.startColor = Color.red;
+            rocketLine.endColor = Color.red;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        Destroy(rocketLine);
+        t = 0;
+        float diveTime = 0.7f;
+        startPos = rocket.transform.position;
+
+        while (t < diveTime)
+        {
+            rocket.transform.position = Vector2.Lerp(startPos, targetPos, t / diveTime);
+            t += Time.deltaTime;
+            yield return null;
+
+        }
+        rocketHitPositions.Remove(rocketHitPos);
+        rocketList.Remove(rocket);
+    }
+}
